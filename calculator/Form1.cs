d5d549cea4a06c6682fef915ffbf1e6630923de4
Hafsa123456789 <41472581+Hafsa123456789@users.noreply.github.com>
2019-04-05 22:28:28 +0500

    Windows Calculator using C# .NET

---
+++ calculator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        Double value = 0;
+        string operation = "";
+        bool operationp = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (result.Text == "0" || operationp)
+               result.Clear();
+            operationp = false;
+            Button b = (Button)sender;
+            if (b.Text == ".")
+            {
+                if (!result.Text.Contains("."))
+                    result.Text = result.Text + b.Text;
+            }
+
+            else
+                result.Text = result.Text + b.Text;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operationp = true;
+            label1.Text = value + " " + operation;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            label1.Text = " ";
+            switch (operation)
+            {
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+       
+    }
+}
